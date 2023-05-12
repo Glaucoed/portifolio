39edda0bed16be5188983487eff16de6dfb3f5f1
Glaucoed <gesverzuthe@gmail.com>
2023-05-12 12:13:34 -0300

    feat: realizado menu para mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './styles/global.css'
+import { useMediaQuery } from 'react-responsive';
 
 import Avatar from './components/Avatar.tsx';
 import Sobre from './components/Sobre.tsx';
@@ -6,17 +7,24 @@ import Skill from './components/Skill.tsx';
 import Projeto from './components/Projeto.tsx';
 import Footer from './components/Footer.tsx';
 import Nav from './components/Nav.tsx';
+import Navbar from './components/Navbar.tsx'
 
 function App() {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
   return (
+    <>
+    {
+      isMobile ? <Navbar /> :  <Nav />
+    }
+    
     <main id="main">
-      <Nav />
       <Avatar />
       <Sobre />
       <Skill />
       <Projeto />
       <Footer />
     </main>
+    </>
   );
 }
 
