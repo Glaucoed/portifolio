7deb48a049dfa8dc063537fd2f73c6d6bfd6e9d3
Glaucoed <gesverzuthe@gmail.com>
2023-05-12 16:57:24 -0300

    feat: adicionado projetos

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -1,57 +1,57 @@
 const projects = [
   {
     id: 1,
-    name: 'Trybe Futebol Clube',
-    data: '27 de Fevereiro 2023',
-    tecnologias: ['react', 'express', 'typescript', 'javascript', 'ORM', 'POO'],
-    descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tortor urna, vestibulum eget ex ac, ornare bibendum metus. In congue tincidunt ante. In ultricies velit a ante consequat, sit amet blandit sem hendrerit. Ut rutrum sodales gravida. Nunc scelerisque ex a nisl auctor mollis. Mauris pretium elit sit amet placerat blandit. Nulla eget erat at risus accumsan malesuada sed ac ipsum.',
-    repositorio: 'https://github.com/Glaucoed/trybe-tfc-node-react',
-    img: 'https://www.leoprado.dev/assets/portfolio-05-a2266788.jpg'
+    name: 'Delivery App',
+    data: '14 de Abril 2023',
+    tecnologias: ['React', 'Express', 'Mysql', 'Node', 'Jest', 'POO', 'Sinnon'],
+    descricao: 'Projeto full stack React/Node.js desenvolvido por Glauco, Pedro, Rayanderson, Francisco e Arthur ao final do Módulo de Back-end para Trybe, o front-end foi desenvolvido em React, enquanto o Back-end utiliza Node.js, Express.js e Sequelize.js',
+    repositorio: 'https://github.com/Glaucoed/trybe-delivery-app-fullstack-react-node-orm',
+    img: 'https://i.ibb.co/WtwynnF/delivery.jpg'
   },
   {
     id: 2,
-    name: 'Trybe Futebol Clube',
-    data: '27 de Fevereiro 2023',
-    tecnologias: ['react', 'express', 'typescript', 'javascript', 'ORM', 'POO'],
-    descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tortor urna, vestibulum eget ex ac, ornare bibendum metus. In congue tincidunt ante. In ultricies velit a ante consequat, sit amet blandit sem hendrerit. Ut rutrum sodales gravida. Nunc scelerisque ex a nisl auctor mollis. Mauris pretium elit sit amet placerat blandit. Nulla eget erat at risus accumsan malesuada sed ac ipsum.',
+    name: 'Shopping Cart',
+    data: '08 de Janeiro 2023',
+    tecnologias: ['Jest','Javascript', 'CSS', 'HTML', 'TDD'],
+    descricao: 'Neste projeto, fiz um carrinho de compras totalmente dinâmico! E o melhor de tudo: vou consumir dados diretamente de uma API do Mercado Livre. A sigla API significa Application Programming Interface (Interface de Programação de Aplicativos) e é um ponto de contato na internet com um determinado serviço. Através de requisições HTTP, posso interagir com a API da forma como foi planejada por quem a criou. No nosso caso, irei utilizar a API do Mercado Livre para buscar produtos à venda',
     repositorio: 'https://github.com/Glaucoed/trybe-tfc-node-react',
-    img: 'https://www.leoprado.dev/assets/portfolio-06-2926a473.jpg'
+    img: 'https://i.ibb.co/XW50hh6/frontend.jpg'
   },
   {
     id: 3,
-    name: 'Trybe Futebol Clube',
-    data: '27 de Fevereiro 2023',
-    tecnologias: ['react', 'express', 'typescript', 'javascript', 'ORM', 'POO'],
-    descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tortor urna, vestibulum eget ex ac, ornare bibendum metus. In congue tincidunt ante. In ultricies velit a ante consequat, sit amet blandit sem hendrerit. Ut rutrum sodales gravida. Nunc scelerisque ex a nisl auctor mollis. Mauris pretium elit sit amet placerat blandit. Nulla eget erat at risus accumsan malesuada sed ac ipsum.',
-    repositorio: 'https://github.com/Glaucoed/trybe-tfc-node-react',
-    img: 'https://www.leoprado.dev/assets/portfolio-05-a2266788.jpg'
+    name: 'App de Receitas',
+    data: '24 de Outubro 2022',
+    tecnologias: ['React', 'HTML', 'CSS', 'Bootstrap', 'Context-API', 'jest', 'RTL'],
+    descricao: 'Nesse projeto foi desenvolvido um app de receitas, utilizando o que há de mais moderno dentro do ecossistema React: Hooks e Context API. Nele será possível: ver, buscar, filtrar, favoritar e acompanhar o progresso de preparação de receitas de comidas e bebidas. A base de dados serão 2 APIs distintas, uma para comidas e outra para bebidas. O layout tem como foco dispositivos móveis, dessa forma todos os protótipos vão estar desenvolvidos em telas menores.',
+    repositorio: 'https://github.com/Glaucoed/trybe-app-de-receitas-contextAPI-react',
+    img: 'https://i.ibb.co/cDgm0fn/receitas.jpg'
   },
   {
     id: 4,
-    name: 'Trybe Futebol Clube',
-    data: '27 de Fevereiro 2023',
-    tecnologias: ['react', 'express', 'typescript', 'javascript', 'ORM', 'POO'],
-    descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tortor urna, vestibulum eget ex ac, ornare bibendum metus. In congue tincidunt ante. In ultricies velit a ante consequat, sit amet blandit sem hendrerit. Ut rutrum sodales gravida. Nunc scelerisque ex a nisl auctor mollis. Mauris pretium elit sit amet placerat blandit. Nulla eget erat at risus accumsan malesuada sed ac ipsum.',
-    repositorio: 'https://github.com/Glaucoed/trybe-tfc-node-react',
-    img: 'https://www.leoprado.dev/assets/portfolio-06-2926a473.jpg'
+    name: 'Solar System',
+    data: '17 de Agosto 2022',
+    tecnologias: ['react','javascript'],
+    descricao: 'Neste projeto, utilizei arquivos simulando o uso de uma API para renderizar uma lista de planetas. A lista exibe informações como nomes, imagens e descrições dos planetas. Para facilitar a implementação, adotamos o uso de componentes modelo padronizados, como cards, títulos e divisões. Dessa forma, garantimos uma apresentação consistente e organizada das informações dos planetas. Foi uma maneira eficiente de criar essa funcionalidade sem depender diretamente de uma API real.',
+    repositorio: 'https://github.com/Glaucoed/trybe-solar-system-react',
+    img: 'https://i.ibb.co/5jwdvCv/solar.jpg'
   },
   {
     id: 5,
-    name: 'Trybe Futebol Clube',
+    name: 'Pixel Art',
     data: '27 de Fevereiro 2023',
-    tecnologias: ['react', 'express', 'typescript', 'javascript', 'ORM', 'POO'],
-    descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tortor urna, vestibulum eget ex ac, ornare bibendum metus. In congue tincidunt ante. In ultricies velit a ante consequat, sit amet blandit sem hendrerit. Ut rutrum sodales gravida. Nunc scelerisque ex a nisl auctor mollis. Mauris pretium elit sit amet placerat blandit. Nulla eget erat at risus accumsan malesuada sed ac ipsum.',
-    repositorio: 'https://github.com/Glaucoed/trybe-tfc-node-react',
-    img: 'https://www.leoprado.dev/assets/portfolio-06-2926a473.jpg'
+    tecnologias: ['CSS', 'HTML', 'Javascript'],
+    descricao: 'Neste projeto, o desafio é implementar um editor de arte com pixels em que a pessoa usuária poderá escolher uma cor em uma paleta de cores e poderá pintar o que quiser em um quadro branco',
+    repositorio: 'https://github.com/Glaucoed/trybe-pixels-art-javascript-html-css',
+    img: 'https://i.ibb.co/thsvVP9/color.jpg'
   },
   {
     id: 6,
     name: 'Trybe Futebol Clube',
     data: '27 de Fevereiro 2023',
-    tecnologias: ['react', 'express', 'typescript', 'javascript', 'ORM', 'POO'],
-    descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tortor urna, vestibulum eget ex ac, ornare bibendum metus. In congue tincidunt ante. In ultricies velit a ante consequat, sit amet blandit sem hendrerit. Ut rutrum sodales gravida. Nunc scelerisque ex a nisl auctor mollis. Mauris pretium elit sit amet placerat blandit. Nulla eget erat at risus accumsan malesuada sed ac ipsum.',
+    tecnologias: ['react', 'typescript', 'ORM Sequelize', 'POO', 'Docker', 'Testes (Sinon, Chai, Mocha)'],
+    descricao: 'Neste projeto, fui responsável pelo desenvolvimento do backend de uma aplicação frontend existente. Minha tarefa envolveu a criação de uma API, utilizando a metodologia TDD (Test-Driven Development). Além disso, integrei a aplicação com o frontend usando o docker-compose, garantindo que ambos funcionem em conjunto, consumindo um banco de dados. Para o desenvolvimento do backend, utilizei o Docker para criar um ambiente isolado e portátil. Utilizei o Sequelize para a modelagem de dados, estabelecendo os relacionamentos necessários entre as tabelas do banco de dados. Foi um desafio interessante construir essa parte do projeto e garantir que o frontend e o backend trabalhassem de forma integrada. Estou satisfeito com o resultado alcançado.',
     repositorio: 'https://github.com/Glaucoed/trybe-tfc-node-react',
-    img: 'https://www.leoprado.dev/assets/portfolio-06-2926a473.jpg'
+    img: 'https://i.ibb.co/tphYKtZ/tfc.jpg'
   }
   ]
   
